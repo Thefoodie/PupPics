4e914980d43e3167babf36759d964919f3569769
Whit Johnson <wbjohnson.4@gmail.com>
2014-02-09 01:21:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puppics::Application.config.secret_key_base = 'f888d489ef6bfdd253132122db95a25089a1cc7d42222d03cc697fe21b89b4ef096ae9b2a0432ce75143e2e77b3e4465f4249d356381553de690ed14db74af67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Puppics::Application.config.session_store :cookie_store, key: '_Puppics_session'
